529fd39ea906ee5c6481a67b2c45f530c7ab94a1
Yoga Baskoro <baskoroyoga40@gmail.com>
2021-09-04 22:18:33 +0700

    Create Today and Upcoming migrations model

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body class="antialiased">
         <div id="app">
-
+            <example-component></example-component>
         </div>
         <script src="{{ asset('js/app.js') }}"></script>
     </body>
